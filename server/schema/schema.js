4dfbaa745816198484dce91c07c8e80e5ae61a45
Shad <Shadifmg@gmail.com>
2025-01-30 21:18:46 -0600

    Feat: Created schema for project and projects to grab that information in the api

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,8 +8,18 @@ const {
   GraphQLList,
 } = require("graphql");
 
-// Client Type
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
 
+// Client Type
 const ClientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
@@ -23,12 +33,28 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
         return clients;
       },
     },
+
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
