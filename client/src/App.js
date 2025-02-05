cffdbc94a9b2f13a5bbc603d388a2032e9b45523
Shad <Shadifmg@gmail.com>
2025-02-04 21:11:25 -0600

    Feat: Added Apolloprovider, apolloclient and inmemorycache, to link the frontend to the graphql api with all the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>are you sure about dat buddy</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <h1>AYYOO??!!!</h1>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
