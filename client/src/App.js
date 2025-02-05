6351eff556c90ba0bdad8da11a890918354975a9
Shad <Shadifmg@gmail.com>
2025-02-05 16:47:46 -0600

    Feat: Added project status to projects. Added pages folder with Home Page, Not Found Page and Project Page. moved clients, projects and add client modal components to home page component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Clients from "./components/Clients";
-import AddClientModal from "./components/AddClientModal";
-import Projects from "./components/Projects";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const cache = new InMemoryCache({
@@ -32,12 +33,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} /> 
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
