40a5f4938a4367c6e7de3cf69290119e2a31258c
Shad <Shadifmg@gmail.com>
2025-02-04 21:45:52 -0600

    Feat: Created ClientRow component. Client grabs data from graphql api and loads it into client and then loads it into client row

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-import { use } from "react";
+import ClientRow from "./ClientRow";
+
 
 const GET_CLIENTS = gql`
   query GetClients {
@@ -18,5 +19,25 @@ export default function Clients() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oops... Something Went Wrong</p>;
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map(client => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 }
