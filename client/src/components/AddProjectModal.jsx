ffef56dfbad28139e32ae1be00109c1c6646bbfa
Shad <Shadifmg@gmail.com>
2025-02-05 18:23:39 -0600

    Feat: Added the Clients dropdown in the New Project form to show any clients that were added

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -3,6 +3,7 @@ import { FaList } from "react-icons/fa";
 import { useMutation, useQuery } from "@apollo/client";
 import { ADD_PROJECT } from "../mutations/projectMutations";
 import { GET_PROJECTS } from "../queries/projectQueries";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 export default function AddProjectModal() {
   const [name, setName] = useState("");
@@ -21,6 +22,9 @@ export default function AddProjectModal() {
     },
   });
 
+  // Get clients for select dropdown
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
   const onSubmit = (e) => {
     e.preventDefault();
     if (name === "" || description === "" || status === "") {
@@ -35,86 +39,110 @@ export default function AddProjectModal() {
     setClientId("");
   };
 
+  if (loading) return null;
+  if (error) return "Something Went Wrong";
+
   return (
     <>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#addProjectModal"
-      >
-        <div className="d-flex align-items-center">
-          <FaList className="icon" />
-          <div>New Project</div>
-        </div>
-      </button>
-
-      <div
-        className="modal fade"
-        id="addProjectModal"
-        aria-labelledby="addProjectModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="addProjectModalLabel">
-                New Project
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+      {!loading && !error && (
+        <>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addProjectModal"
+          >
+            <div className="d-flex align-items-center">
+              <FaList className="icon" />
+              <div>New Project</div>
             </div>
-            <div className="modal-body">
-              <form onSubmit={onSubmit}>
-                <div className="mb-3">
-                  <label className="form-label">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Description</label>
-                  <textarea
-                    className="form-control"
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                  ></textarea>
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Status</label>
-                  <select
-                    className="form-select"
-                    id="status"
-                    value={status}
-                    onChange={(e) => setStatus(e.target.value)}
-                  >
-                    <option value="new">Not Started</option>
-                    <option value="progress">In Progress</option>
-                    <option value="completed">Completed</option>
-                  </select>
-                </div>
+          </button>
 
-                <button
-                  type="Submit"
-                  data-bs-dismiss="modal"
-                  className="btn btn-primary"
-                >
-                  Submit
-                </button>
-              </form>
+          <div
+            className="modal fade"
+            id="addProjectModal"
+            aria-labelledby="addProjectModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="addProjectModalLabel">
+                    New Project
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Description</label>
+                      <textarea
+                        className="form-control"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      ></textarea>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Status</label>
+                      <select
+                        className="form-select"
+                        id="status"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                      >
+                        <option value="new">Not Started</option>
+                        <option value="progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </select>
+                    </div>
+
+                    <div className="mb-3">
+                      <label className="form-label">Client</label>
+                      <select
+                        id="clientId"
+                        className="form-select"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                      >
+                        <option value="">Select Client</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <button
+                      type="Submit"
+                      data-bs-dismiss="modal"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
